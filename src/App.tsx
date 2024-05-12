da3ab5aaa48156c147ff29b728d771a3eeea80a1
Chris Pence <chris@pencels.net>
2024-05-11 17:49:32 -0700

    Add usage info

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,36 @@ import Listener from "./Listener";
 
 function App() {
   return (
-    <div className="container mx-auto p-2 grid gap-2">
+    <div className="container mx-auto p-2 flex flex-col gap-2">
       <Listener />
       <div className="grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 gap-2">
         <TwitchPanel />
         <SpotifyPanel />
+      </div>
+      <div className="lg:mt-3 mx-auto p-3 prose prose-invert prose-sm">
+        <h2>How do I use this?</h2>
+        <ol>
+          <li>
+            Click "Connect with Twitch" and "Connect with Spotify" to connect
+            and authorize this app.
+          </li>
+          <li>
+            Select the Twitch channel point redeem to use for song requests.
+          </li>
+          <li>Leave this page open.</li>
+          <li>You now have automatic song request redeems!</li>
+        </ol>
+        <h2>How it works</h2>
+        <p>
+          At a high level, this app listens for redemption events from your
+          Twitch channel, looks up a song on Spotify based off the redemption
+          input text, then adds that song to your Spotify queue.
+        </p>
+        <p>
+          This all happens locally in your browser. There is no server
+          component, so song redeem functionality goes away once you close this
+          page.
+        </p>
       </div>
     </div>
   );
